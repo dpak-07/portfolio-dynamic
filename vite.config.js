c16a402325ad57a158c3a7496253f64a246daf93
DEEPAK <deepakofficial0103@gmail.com>
2025-07-29 11:58:02 +0530

    final pusk

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,   // allows LAN access (e.g. phone on same Wi-Fi)
+    port: 3000    // use desired port here
+  }
 })
