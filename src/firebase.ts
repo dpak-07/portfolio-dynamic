74e8425416ebd8512000be6998352c86619c6b14
DEEPAK <deepakofficial0103@gmail.com>
2026-02-14 00:29:13 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -10,7 +10,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-
+ 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export default app;
